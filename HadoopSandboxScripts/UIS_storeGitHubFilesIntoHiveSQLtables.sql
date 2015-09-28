d66fb4cbcee318c9516fdea426118674df80d23c
Øyvind Blaauw <oeblaauw@users.noreply.github.com>
2015-09-28 12:25:33 +0200

    storeGitHubFilesIntoHiveSQLtables to UiS cluster

---
+++ HadoopSandboxScripts/UIS_storeGitHubFilesIntoHiveSQLtables.sql
@@ -0,0 +1,65 @@
+--create table and import data
+--ratings
+DROP TABLE IF EXISTS ratings;
+
+CREATE EXTERNAL TABLE IF NOT EXISTS ratings (
+userId BIGINT, 
+movieId BIGINT, 
+rating DOUBLE,
+`timestamp` BIGINT)
+ROW FORMAT DELIMITED 
+FIELDS TERMINATED BY ',' 
+LINES TERMINATED BY '\n'
+STORED AS TEXTFILE;
+
+LOAD DATA INPATH '/mnt/data/ratings.csv' OVERWRITE INTO TABLE ratings;
+
+--create new date features in a new rating table derived from ratings
+DROP TABLE IF EXISTS ratingsTimestamp2;
+
+CREATE TABLE ratingsTimestamp2 AS
+SELECT 
+*, 
+from_unixtime(`timestamp`) as new_timestamp,
+year(from_unixtime(`timestamp`)) as yearOfRating,
+month(from_unixtime(`timestamp`)) as monthOfRating,
+day(from_unixtime(`timestamp`)) as dayOfRating,
+hour(from_unixtime(`timestamp`)) as hourOfRating
+from ratings;
+
+--movies
+CREATE EXTERNAL TABLE IF NOT EXISTS movies (
+movieId BIGINT,
+title STRING,
+genres STRING)
+ROW FORMAT DELIMITED 
+FIELDS TERMINATED BY ',' 
+LINES TERMINATED BY '\n'
+STORED AS TEXTFILE;
+
+LOAD DATA INPATH '/mnt/data/movies.csv' OVERWRITE INTO TABLE movies;
+
+--tags
+CREATE EXTERNAL TABLE IF NOT EXISTS tags (
+userId BIGINT,
+movieId BIGINT,
+tag STRING,
+`timestamp` BIGINT)
+ROW FORMAT DELIMITED 
+FIELDS TERMINATED BY ',' 
+LINES TERMINATED BY '\n'
+STORED AS TEXTFILE;
+
+LOAD DATA INPATH '/mnt/data/tags.csv' OVERWRITE INTO TABLE tags;
+
+--links
+CREATE EXTERNAL TABLE IF NOT EXISTS links (
+movieId BIGINT,
+imdbId BIGINT,
+tmdbId BIGINT)
+ROW FORMAT DELIMITED 
+FIELDS TERMINATED BY ',' 
+LINES TERMINATED BY '\n'
+STORED AS TEXTFILE;
+
+LOAD DATA INPATH '/mnt/data/links.csv' OVERWRITE INTO TABLE links;
